9d164c4f8b3f96ab2bfe1e8a57b3f1a6c3195d42
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-08-01 18:55:46 -0300

    Requisito 5

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      artist: '',
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    this.setState({
+      artist: target.value }, () => {
+      const MIN_CHARACTERS = 2;
+      const isValid = target.value.length < MIN_CHARACTERS;
+      this.setState({ isDisabled: isValid });
+    });
+  }
+
   render() {
+    const { isDisabled, artist } = this.state;
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        <Header />
+        <label htmlFor="artist">
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ artist }
+            onChange={ this.onInputChange }
+          />
+        </label>
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ isDisabled }
+        >
+          Pesquisar
+
+        </button>
+      </div>
     );
   }
 }

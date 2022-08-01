8a937b25b50751536375aa914918aba873242a75
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-08-01 18:40:19 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
+import Favorites from './Favorites';
+import Profile from './Profile';
+import Search from './Search';
 
 class Header extends React.Component {
   constructor() {
@@ -29,6 +33,11 @@ class Header extends React.Component {
     return (
       <div data-testid="header-component">
         <span data-testid="header-user-name">{loading ? 'Carregando...' : user}</span>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">{Search}</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">{Favorites}</Link>
+          <Link data-testid="link-to-profile" to="/profile">{Profile}</Link>
+        </nav>
       </div>
     );
   }

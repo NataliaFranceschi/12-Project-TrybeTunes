46741d82f8f477ce3a9a586a1d0047e5047474c4
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-08-03 17:23:46 -0300

    Requisito 7

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,11 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Header from './Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from './MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      albumInformation: undefined,
+      loading: false,
+      musics: [],
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { match: { params: { id } } } = this.props;
+        const resposta = await getMusics(id);
+        const musics = resposta
+          .filter(({ kind }) => kind === 'song');
+        this.setState({
+          albumInformation: resposta[0],
+          loading: false,
+          musics: [...musics],
+        });
+      },
+    );
+  }
+
+  Card = () => {
+    const { albumInformation, musics } = this.state;
+    if (albumInformation !== undefined) {
+      return (
+        <div>
+          <p data-testid="artist-name">
+            {albumInformation.artistName}
+          </p>
+          <p data-testid="album-name">
+            {albumInformation.collectionName}
+          </p>
+          <MusicCard musics={ musics } />
+        </div>
+      );
+    }
+  }
+
   render() {
+    const { loading } = this.state;
     return (
-      <div data-testid="page-album" />
+      <div data-testid="page-album">
+        <Header />
+        <section>{loading ? 'Carregando...' : this.Card()}</section>
+      </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired };
 
 export default Album;
